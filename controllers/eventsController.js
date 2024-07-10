39adc79b6bff97ad02cdc39f4167935326756941
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-10 08:19:10 -0300

    add endpoints allow rsvps

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -16,3 +16,24 @@ export const getEvents = async (req, res) => {
     return res.status(500).send({ message: "An error occurred on the server" });
   }
 };
+
+export const eventRSVP = async (req, res) => {
+  const { id } = req.params;
+  const { response } = req.body;
+
+  const payload = req.tokenPayload.id;
+
+  const eventRSVP = {
+    user_id: payload.id,
+    event_id: id,
+    status: response
+  }
+
+  try {
+    const rsvpId = await knex("event_rsvps").insert(eventRSVP);
+    const addedRsvp = { id: rsvpId[0], event_id: id, response: response };
+    res.status(201).send(addedRsvp);
+  } catch (err) {
+    return res.status(500).send({ message: "An error occurred on the server" });
+  }
+};
--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,11 @@
 import express from "express";
+import auth from "../middleware/auth.js";
 
-import {getEvents} from "../controllers/eventsController.js"
+import { getEvents, eventRSVP } from "../controllers/eventsController.js";
 
 const eventsRouter = express.Router();
 
-eventsRouter.get("/", getEvents);
+eventsRouter.get("/", auth, getEvents);
+eventsRouter.post("/:id/rsvps", auth, eventRSVP);
 
 export default eventsRouter;
