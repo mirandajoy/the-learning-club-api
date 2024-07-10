dd10c9e94afcff956407cc9e90b9f9504a8157de
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-09 21:08:55 -0300

    allow users to join groups, update group endpoint to show joined groups, add endpoint to get events for a specific group

--- controllers/groupsControllers.js
+++ controllers/groupsControllers.js
@@ -6,9 +6,66 @@ import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
 export const getGroups = async (req, res) => {
+  if (!req.headers.authorization) {
+    try {
+      const groups = await knex
+        .select("id", "city", "state", "country", "remote")
+        .from("groups")
+        .where({ remote: 0 })
+        .orderBy("city");
+      return res.status(200).send(groups);
+    } catch (err) {
+      return res.status(500).send({ message: "An error occurred on the server" });
+    }
+  }
+
+  const authHeader = req.headers.authorization;
+  const authToken = authHeader.split(" ")[1];
+  const payload = jwt.verify(authToken, process.env.JWT_KEY);
+
   try {
-    const groups = await knex.select("id", "city", "state", "country", "remote").from("groups").orderBy("city");
+    const groups = await knex
+      .with("groups_joined", (qb) => {
+        qb.select("group_id").from("group_members").where({ user_id: payload.id }).groupBy("group_id");
+      })
+      .select("*")
+      .from("groups_joined")
+      .rightJoin("groups", "groups.id", "groups_joined.group_id")
+      .where({ remote: 0 });
     res.status(200).send(groups);
+  } catch (err) {
+    return res.status(500).send({ message: "An error occurred on the server" });
+  }
+};
+
+export const joinGroup = async (req, res) => {
+  const { id } = req.params;
+  const authHeader = req.headers.authorization;
+  const authToken = authHeader.split(" ")[1];
+
+  const payload = jwt.verify(authToken, process.env.JWT_KEY);
+
+  const newMember = {
+    group_id: id,
+    user_id: payload.id,
+    role: "member",
+  };
+
+  try {
+    const memberId = await knex("group_members").insert(newMember);
+    const addedMember = { id: memberId[0], group_id: id, role: "member" };
+    res.status(201).send(addedMember);
+  } catch (err) {
+    return res.status(500).send({ message: "An error occurred on the server" });
+  }
+};
+
+export const getGroupEvents = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const events = await knex.select("id", "location", "time").from("events").where({ group_id: id }).orderBy("time");
+    res.status(200).send(events);
   } catch (err) {
     return res.status(500).send({ message: "An error occurred on the server" });
   }
--- routes/groups.js
+++ routes/groups.js
@@ -1,9 +1,11 @@
 import express from "express";
 
-import {getGroups} from "../controllers/groupsControllers.js";
+import {getGroups, joinGroup, getGroupEvents} from "../controllers/groupsControllers.js";
 
 const groupsRouter = express.Router();
 
 groupsRouter.get("/", getGroups);
+groupsRouter.post("/:id/members", joinGroup);
+groupsRouter.get("/:id/events", getGroupEvents);
 
 export default groupsRouter;
