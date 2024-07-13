0c27e111857e49057527c7d41816fbe41fbf5756
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-13 11:22:40 -0300

    update auth middleware to handle calls where no auth header is provided

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,11 @@ import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
   const authHeader = req.headers.authorization;
-  const authToken = authHeader.split(" ")[1];
-  req.tokenPayload = jwt.verify(authToken, process.env.JWT_KEY);
+
+  if (authHeader !== undefined) {
+    const authToken = authHeader.split(" ")[1];
+    req.tokenPayload = jwt.verify(authToken, process.env.JWT_KEY);
+  }
 
   next();
 };
