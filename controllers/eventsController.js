107d83cb9246e25e434b914b6169e2a97833de30
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-07-13 11:14:14 -0300

    add endpoints to get a single event and update an event rsvp

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -6,12 +6,32 @@ const knex = initKnex(configuration);
 
 export const getEvents = async (req, res) => {
   try {
-    const events = await knex
-      .select("id", "location", "time")
-      .from("events")
-      .where({ group_id: groupId })
-      .first();
+    const events = await knex.select("id", "location", "time").from("events").where({ group_id: groupId }).first();
     res.status(200).send(events);
+  } catch (err) {
+    return res.status(500).send({ message: "An error occurred on the server" });
+  }
+};
+
+export const getSingleEvent = async (req, res) => {
+  const { id } = req.params;
+  const payload = req.tokenPayload;
+
+  try {
+    const singleEvent = await knex
+      .with("rsvp", (qb) => {
+        qb.select("id", "event_id", "status")
+          .from("event_rsvps")
+          .where({ user_id: payload.id });
+      })
+      .select("events.id", "location", "address", "time", "status", "rsvp.id as rsvp_id")
+      .from("events")
+      .leftJoin("rsvp", "events.id", "rsvp.event_id")
+      .where({"events.id": id })
+      .first();
+
+    console.log(singleEvent);
+    res.status(200).send(singleEvent);
   } catch (err) {
     return res.status(500).send({ message: "An error occurred on the server" });
   }
@@ -26,13 +46,34 @@ export const eventRSVP = async (req, res) => {
   const eventRSVP = {
     user_id: payload.id,
     event_id: id,
-    status: response
-  }
+    status: response,
+  };
 
   try {
     const rsvpId = await knex("event_rsvps").insert(eventRSVP);
     const addedRsvp = { id: rsvpId[0], event_id: id, response: response };
     res.status(201).send(addedRsvp);
+  } catch (err) {
+    return res.status(500).send({ message: "An error occurred on the server" });
+  }
+};
+
+export const updateRSVP = async (req, res) => {
+  const { id, rsvpId } = req.params;
+  const { response } = req.body;
+
+  const payload = req.tokenPayload;
+
+  const eventRSVP = {
+    user_id: payload.id,
+    event_id: id,
+    status: response,
+  };
+
+  try {
+    const rsvpDBId = await knex("event_rsvps").where({ id: rsvpId }).update(eventRSVP);
+    const updatedRsvp = { id: rsvpDBId[0], event_id: id, response: response };
+    res.status(201).send(updatedRsvp);
   } catch (err) {
     return res.status(500).send({ message: "An error occurred on the server" });
   }
--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,13 @@
 import express from "express";
 import auth from "../middleware/auth.js";
 
-import { getEvents, eventRSVP } from "../controllers/eventsController.js";
+import { getEvents, getSingleEvent, eventRSVP, updateRSVP } from "../controllers/eventsController.js";
 
 const eventsRouter = express.Router();
 
 eventsRouter.get("/", auth, getEvents);
+eventsRouter.get("/:id", auth, getSingleEvent);
 eventsRouter.post("/:id/rsvps", auth, eventRSVP);
+eventsRouter.put("/:id/rsvps/:rsvpId", auth, updateRSVP);
 
 export default eventsRouter;
